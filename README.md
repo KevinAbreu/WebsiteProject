b3899062aa789834e449b5ea2e999c7837b4d43b
Kevin Abreu <kevinrabreu@hotmail.com>
2019-05-15 08:10:55 -0400

    Complete I guess

---
+++ README.md
@@ -0,0 +1,6 @@
+<!-- link to wireframe -->
+https://ibb.co/KVzZTTF
+
+<!-- final thoughts -->
+
+<!-- I have to remind myself that backend comes first before frontend. I'm way too sloppy... -->
---
+++ main.js
@@ -0,0 +1,23 @@
+  var logobtn = document.getElementById('logo')
+  var list = document.getElementById("list")
+  var list2 = document.getElementById("list2")
+  var list3 = document.getElementById("list3")
+  var list4 = document.getElementById("list4")
+
+logobtn.addEventListener('click', function(){
+  list.style.marginLeft = '90px';
+  list.style.opacity = '1';
+  list2.style.marginLeft = '200px';
+  list2.style.opacity = '1';
+  list2.style.color = 'white';
+  list3.style.marginLeft = '90px';
+  list3.style.opacity = '1';
+  list3.style.color = 'white';
+  list4.style.marginLeft = '90px';
+  list4.style.opacity = '1';
+  list4.style.color = 'white';
+  list.style.display='inline'
+  list2.style.display='inline'
+  list3.style.display='inline'
+  list4.style.display='inline'
+})
